d338b1044f053524b9925a544c067491a014781d
Fabien Potencier <fabien@potencier.org>
2022-02-13 09:36:51 +0100

    Add 9.2: Configuring EasyAdmin

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -32,6 +32,11 @@ class Comment
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $photoFilename;
 
+    public function __toString(): string
+    {
+        return (string) $this->getEmail();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
